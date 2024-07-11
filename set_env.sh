5ecad5d6ee06f5436bc315fb9c7dbfcb118c2fe1
Jainil Patel <jainilpatel115@gmail.com>
2024-07-11 12:08:50 +0000

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -5,7 +5,7 @@ echo IMAGE_TAG=$IMAGE_TAG >> .env
 
 # Add variables here
 echo PORT=$PORT >> .env
-echo MONGODB_URL=$MONGODB_URI >> .env
+echo MONGO_URL=$MONGO_URL >> .env
 echo ACCESS_TOKEN_SECRET=$ACCESS_TOKEN_SECRET >> .env
 echo REFRESH_TOKEN_SECRET=$REFRESH_TOKEN_SECRET >> .env
 echo FRONTEND_URL=$FRONTEND_URL >> .env
