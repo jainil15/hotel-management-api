029f1e2d2221937868c551e0353ce3a8dc244a4a
Jainil <jainilpatel115@gmail.com>
2024-08-20 20:10:03 +0530

    Solution for us phonenumber

--- src/controllers/twilio.controller.js
+++ src/controllers/twilio.controller.js
@@ -13,21 +13,21 @@ const { TwilioAccount } = require("../models/twilioAccount.model");
 const { Property } = require("../models/property.model");
 const { responseHandler } = require("../middlewares/response.middleware");
 const {
-	NotFoundError,
-	APIError,
-	ValidationError,
-	BadRequestError,
-	InternalServerError,
-	ConflictError,
+  NotFoundError,
+  APIError,
+  ValidationError,
+  BadRequestError,
+  InternalServerError,
+  ConflictError,
 } = require("../lib/CustomErrors");
 const { mongo, default: mongoose } = require("mongoose");
 const {
-	messageTriggerType,
-	messageType,
+  messageTriggerType,
+  messageType,
 } = require("../constants/message.constant");
 const {
-	TWILIO_ACCOUNT_SID,
-	TWILIO_AUTH_TOKEN,
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
 } = require("../constants/twilio.constant");
 
 /**
@@ -38,20 +38,21 @@ const {
  * @returns {import('express').Response} - The response
  */
 const getPhoneNumbers = async (req, res, next) => {
-	try {
-		const country = req.query.country;
-		const allPhoneNumbers = await twilioService.getPhoneNumbers(country);
-		const phoneNumbers = allPhoneNumbers.map((phoneNumber) => ({
-			phoneNumber: phoneNumber.phone_number,
-			friendlyName: phoneNumber.friendly_name,
-		}));
-		return responseHandler(res, { phoneNumbers: phoneNumbers });
-	} catch (e) {
-		if (e instanceof APIError) {
-			return next(e);
-		}
-		return next(new InternalServerError());
-	}
+  try {
+    const country = req.query.country;
+    const allPhoneNumbers = await twilioService.getPhoneNumbers(country);
+    console.log(allPhoneNumbers);
+    const phoneNumbers = allPhoneNumbers.map((phoneNumber) => ({
+      phoneNumber: phoneNumber.phone_number,
+      friendlyName: phoneNumber.friendly_name,
+    }));
+    return responseHandler(res, { phoneNumbers: phoneNumbers });
+  } catch (e) {
+    if (e instanceof APIError) {
+      return next(e);
+    }
+    return next(new InternalServerError());
+  }
 };
 
 /**
@@ -62,53 +63,53 @@ const getPhoneNumbers = async (req, res, next) => {
  * @returns {import('express').Response} - The response
  */
 const buyPhoneNumber = async (req, res, next) => {
-	try {
-		const propertyId = req.params.propertyId;
-		const phoneNumber = req.body.phoneNumber;
+  try {
+    const propertyId = req.params.propertyId;
+    const phoneNumber = req.body.phoneNumber;
 
-		const property = await Property.findById(propertyId);
-		if (!property) {
-			throw new NotFoundError("Property not found", {});
-		}
-		// Check if property exists
-		const twilioAccount = await TwilioAccount.findOne({
-			propertyId: propertyId,
-		});
-		if (!twilioAccount) {
-			throw new NotFoundError("Twilio Account not found", {});
-		}
-		// Check if Twilio Account already has a phone number
-		if (twilioAccount.phoneNumber) {
-			throw new ConflictError("Twilio Account already has a phone number", {
-				phoneNumber: ["Twilio Account already has a phone number"],
-			});
-		}
-		const result = z
-			.object({
-				phoneNumber: z.string().refine((val) => phoneregex.test(val), {
-					message: "Invalid phone number format",
-				}),
-			})
-			.safeParse({ phoneNumber });
-		if (!result.success) {
-			throw new ValidationError(
-				"Validation Error",
-				result.error.flatten().fieldErrors,
-			);
-		}
-		const boughtPhoneNumber = await twilioService.buyPhoneNumber(
-			propertyId,
-			phoneNumber,
-			req.user,
-		);
+    const property = await Property.findById(propertyId);
+    if (!property) {
+      throw new NotFoundError("Property not found", {});
+    }
+    // Check if property exists
+    const twilioAccount = await TwilioAccount.findOne({
+      propertyId: propertyId,
+    });
+    if (!twilioAccount) {
+      throw new NotFoundError("Twilio Account not found", {});
+    }
+    // Check if Twilio Account already has a phone number
+    if (twilioAccount.phoneNumber) {
+      throw new ConflictError("Twilio Account already has a phone number", {
+        phoneNumber: ["Twilio Account already has a phone number"],
+      });
+    }
+    const result = z
+      .object({
+        phoneNumber: z.string().refine((val) => phoneregex.test(val), {
+          message: "Invalid phone number format",
+        }),
+      })
+      .safeParse({ phoneNumber });
+    if (!result.success) {
+      throw new ValidationError(
+        "Validation Error",
+        result.error.flatten().fieldErrors,
+      );
+    }
+    const boughtPhoneNumber = await twilioService.buyPhoneNumber(
+      propertyId,
+      phoneNumber,
+      req.user,
+    );
 
-		return responseHandler(res, {}, 201, "Successfully bought phonenumber");
-	} catch (e) {
-		if (e instanceof APIError) {
-			return next(e);
-		}
-		return next(new InternalServerError());
-	}
+    return responseHandler(res, {}, 201, "Successfully bought phonenumber");
+  } catch (e) {
+    if (e instanceof APIError) {
+      return next(e);
+    }
+    return next(new InternalServerError());
+  }
 };
 
 /**
@@ -119,28 +120,27 @@ const buyPhoneNumber = async (req, res, next) => {
  * @returns {import('express').Response} - The response
  */
 const createSubaccount = async (req, res, next) => {
-	try {
-		const propertyId = req.params.propertyId;
-		if (!propertyId) {
-			throw new BadRequestError("Property ID is required", {
-				propertyId: ["Property ID is required"],
-			});
-		}
-		const subaccount = await twilioService.createSubaccount(propertyId);
-		return responseHandler(
-			res,
-			{ subaccount },
-			201,
-			"Twilio subaccount Created",
-		);
-	} catch (e) {
-		if (e instanceof APIError) {
-			return next(e);
-		}
-		return next(new InternalServerError());
-	}
+  try {
+    const propertyId = req.params.propertyId;
+    if (!propertyId) {
+      throw new BadRequestError("Property ID is required", {
+        propertyId: ["Property ID is required"],
+      });
+    }
+    const subaccount = await twilioService.createSubaccount(propertyId);
+    return responseHandler(
+      res,
+      { subaccount },
+      201,
+      "Twilio subaccount Created",
+    );
+  } catch (e) {
+    if (e instanceof APIError) {
+      return next(e);
+    }
+    return next(new InternalServerError());
+  }
 };
-
 
 /**
  * Get toll free verification status from Twilio
@@ -150,54 +150,52 @@ const createSubaccount = async (req, res, next) => {
  * @returns {import('express').Response} - The response
  */
 const getTollFreeVerificationStatus = async (req, res, next) => {
-	try {
-		const propertyId = req.params.propertyId;
+  try {
+    const propertyId = req.params.propertyId;
 
-		const tollFreeVerificationStatus =
-			await twilioService.getTollFreeVerificationStatus(propertyId);
+    const tollFreeVerificationStatus =
+      await twilioService.getTollFreeVerificationStatus(propertyId);
 
-		return responseHandler(res, { tollFreeVerificationStatus });
-	} catch (e) {
-		if (e instanceof APIError) {
-			return next(e);
-		}
-		return next(new InternalServerError());
-	}
+    return responseHandler(res, { tollFreeVerificationStatus });
+  } catch (e) {
+    if (e instanceof APIError) {
+      return next(e);
+    }
+    return next(new InternalServerError());
+  }
 };
-
 
 /**
  * Send message to guest from property - Maybe not used
- * @deprecated 
+ * @deprecated
  * @param {import('express').Request } req - The request
  * @param {import('express').Response} res - The response
  * @param {import('express').NextFunction} next - The next function
  * @returns {import('express').Response} - The response
  */
 const sendMessage = async (req, res, next) => {
-	const session = await mongoose.startSession();
-	session.startTransaction();
-	try {
-		const propertyId = req.params.propertyId;
-		const guestId = req.body.guestId;
-		const body = req.body.body;
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const propertyId = req.params.propertyId;
+    const guestId = req.body.guestId;
+    const body = req.body.body;
 
-		const newMessage = await twilioService.sendMessage(
-			propertyId,
-			guestId,
-			body,
-			session,
-		);
+    const newMessage = await twilioService.sendMessage(
+      propertyId,
+      guestId,
+      body,
+      session,
+    );
 
-		return responseHandler(res, {}, 201, "Message sent successfully");
-	} catch (e) {
-		if (e instanceof APIError) {
-			return next(e);
-		}
-		return next(new InternalServerError(e.message));
-	}
+    return responseHandler(res, {}, 201, "Message sent successfully");
+  } catch (e) {
+    if (e instanceof APIError) {
+      return next(e);
+    }
+    return next(new InternalServerError(e.message));
+  }
 };
-
 
 /**
  * Incoming message from Twilio - Maybe not used
@@ -208,75 +206,74 @@ const sendMessage = async (req, res, next) => {
  * @returns {import('express').Response} - The response
  */
 const incomingMessage = async (req, res, next) => {
-	const session = await mongoose.startSession();
-	session.startTransaction();
-	try {
-		const { From, To, Body } = req.body;
-		const message = {
-			from: From,
-			to: To,
-			body: Body,
-		};
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const { From, To, Body } = req.body;
+    const message = {
+      from: From,
+      to: To,
+      body: Body,
+    };
 
-		const property = await propertyService.find({
-			propertyId: To.substring(req.body.To.length - 10),
-		});
-		if (!property) {
-			throw new NotFoundError("Property not found", {
-				propertyId: ["Property not found for the given id"],
-			});
-		}
-		const guest = await guestService.find({
-			phoneNumber: From.substring(req.body.To.length - 10),
-			propertyId: property._id,
-		});
+    const property = await propertyService.find({
+      propertyId: To.substring(req.body.To.length - 10),
+    });
+    if (!property) {
+      throw new NotFoundError("Property not found", {
+        propertyId: ["Property not found for the given id"],
+      });
+    }
+    const guest = await guestService.find({
+      phoneNumber: From.substring(req.body.To.length - 10),
+      propertyId: property._id,
+    });
 
-		if (!guest) {
-			throw new NotFoundError("Guest not found", {
-				guestId: ["Guest not found for the given id"],
-			});
-		}
+    if (!guest) {
+      throw new NotFoundError("Guest not found", {
+        guestId: ["Guest not found for the given id"],
+      });
+    }
 
-		const newMessage = await messageService.create({
-			propertyId: property._id,
-			guestId: guest._id,
-			senderId: guest._id,
-			receiverId: property._id,
-			content: Body,
-			messageTriggerType: messageTriggerType.MANUAL,
-			messageType: messageType.SMS,
-		});
+    const newMessage = await messageService.create({
+      propertyId: property._id,
+      guestId: guest._id,
+      senderId: guest._id,
+      receiverId: property._id,
+      content: Body,
+      messageTriggerType: messageTriggerType.MANUAL,
+      messageType: messageType.SMS,
+    });
 
-		const updatedChatList = await chatListService.updateAndIncUnreadMessages(
-			guest._id,
-			property._id,
-			{
-				latestMessage: newMessage._id,
-			},
-			session,
-		);
+    const updatedChatList = await chatListService.updateAndIncUnreadMessages(
+      guest._id,
+      property._id,
+      {
+        latestMessage: newMessage._id,
+      },
+      session,
+    );
 
-		req.app.io.to(`guest:${guest._id}`).emit("message:newMessage", {
-			message: newMessage,
-		});
+    req.app.io.to(`guest:${guest._id}`).emit("message:newMessage", {
+      message: newMessage,
+    });
 
-		req.app.io.to(`property:${property._id}`).emit("chatList:update", {
-			chatList: updatedChatList,
-		});
+    req.app.io.to(`property:${property._id}`).emit("chatList:update", {
+      chatList: updatedChatList,
+    });
 
-		await session.commitTransaction();
-		session.endSession();
-		return responseHandler(res, { message });
-	} catch (e) {
-		await session.abortTransaction();
-		session.endSession();
-		if (e instanceof APIError) {
-			return next(e);
-		}
-		return next(new InternalServerError());
-	}
+    await session.commitTransaction();
+    session.endSession();
+    return responseHandler(res, { message });
+  } catch (e) {
+    await session.abortTransaction();
+    session.endSession();
+    if (e instanceof APIError) {
+      return next(e);
+    }
+    return next(new InternalServerError());
+  }
 };
-
 
 /**
  * Get subaccount billing from Twilio
@@ -286,36 +283,36 @@ const incomingMessage = async (req, res, next) => {
  * @returns {import('express').Response} - The response
  */
 const subaccountBilling = async (req, res, next) => {
-	try {
-		const { propertyId } = req.params;
-		const twilioAccount =
-			await twilioAccountService.getByPropertyId(propertyId);
-		if (!twilioAccount) {
-			throw new NotFoundError("Twilio Account not found", {
-				propertyId: ["Twilio account found for the given property id"],
-			});
-		}
-		// TODO: Move client gen to twilio.service
-		const twilioSubClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, {
-			accountSid: twilioAccount.sid,
-		});
+  try {
+    const { propertyId } = req.params;
+    const twilioAccount =
+      await twilioAccountService.getByPropertyId(propertyId);
+    if (!twilioAccount) {
+      throw new NotFoundError("Twilio Account not found", {
+        propertyId: ["Twilio account found for the given property id"],
+      });
+    }
+    // TODO: Move client gen to twilio.service
+    const twilioSubClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, {
+      accountSid: twilioAccount.sid,
+    });
 
-		const billing = await twilioService.subaccountBilling(twilioSubClient);
-		return responseHandler(res, { billing });
-	} catch (e) {
-		if (e instanceof APIError) {
-			return next(e);
-		}
-		return next(new InternalServerError(e.message));
-	}
+    const billing = await twilioService.subaccountBilling(twilioSubClient);
+    return responseHandler(res, { billing });
+  } catch (e) {
+    if (e instanceof APIError) {
+      return next(e);
+    }
+    return next(new InternalServerError(e.message));
+  }
 };
 
 module.exports = {
-	getPhoneNumbers,
-	buyPhoneNumber,
-	createSubaccount,
-	getTollFreeVerificationStatus,
-	sendMessage,
-	incomingMessage,
-	subaccountBilling,
+  getPhoneNumbers,
+  buyPhoneNumber,
+  createSubaccount,
+  getTollFreeVerificationStatus,
+  sendMessage,
+  incomingMessage,
+  subaccountBilling,
 };
--- src/services/twilio.service.js
+++ src/services/twilio.service.js
@@ -12,132 +12,124 @@ const { Guest } = require("../models/guest.model");
 const { Message } = require("../models/message.model");
 
 const getPhoneNumbers = async (country) => {
-	const response = await twilioClient.availablePhoneNumbers(country).fetch();
-	const phoneNumbers = await axios.get(
-		`https://api.twilio.com/${response.subresourceUris.toll_free}`,
-		{
-			auth: {
-				username: accountSid,
-				password: authToken,
-			},
-		},
-	);
-
-	return phoneNumbers.data.available_phone_numbers.filter(
-		(phoneNumber) =>
-			phoneNumber.capabilities.voice === true &&
-			phoneNumber.capabilities.SMS === true &&
-			phoneNumber.capabilities.MMS === true &&
-			phoneNumber.address_requirements === "none",
-	);
+  const response = await twilioClient.availablePhoneNumbers(country).fetch();
+  const phoneNumbers = await axios.get(
+    `https://api.twilio.com/${response.subresourceUris.toll_free}?SMSEnabled=true&VoiceEnabled=true`,
+    {
+      auth: {
+        username: accountSid,
+        password: authToken,
+      },
+    },
+  );
+  return phoneNumbers.data.available_phone_numbers;
 };
 
 const buyPhoneNumber = async (propertyId, phoneNumber, user) => {
-	const property = await Property.findById(propertyId);
-	if (!property) {
-		throw new NotFoundError("Property not found", {
-			propertyId: ["Property not found for the given id"],
-		});
-	}
-	// Check if property exists
-	const twilioAccount = await TwilioAccount.findOne({
-		propertyId: propertyId,
-	});
-	if (!twilioAccount) {
-		throw new NotFoundError("Twilio Account not found", {
-			propertyId: ["Twilio account found for the given property id"],
-		});
-	}
-	// Check if Twilio Account already has a phone number
-	if (twilioAccount.phoneNumber) {
-		throw new ConflictError("Twilio Account already has a phone number", {
-			phoneNumber: ["Twilio Account already has a phone number"],
-		});
-	}
+  const property = await Property.findById(propertyId);
+  if (!property) {
+    throw new NotFoundError("Property not found", {
+      propertyId: ["Property not found for the given id"],
+    });
+  }
+  // Check if property exists
+  const twilioAccount = await TwilioAccount.findOne({
+    propertyId: propertyId,
+  });
+  if (!twilioAccount) {
+    throw new NotFoundError("Twilio Account not found", {
+      propertyId: ["Twilio account found for the given property id"],
+    });
+  }
+  // Check if Twilio Account already has a phone number
+  if (twilioAccount.phoneNumber) {
+    throw new ConflictError("Twilio Account already has a phone number", {
+      phoneNumber: ["Twilio Account already has a phone number"],
+    });
+  }
 
-	// Buy phone number
-	const incomingPhoneNumber = await twilioClient.incomingPhoneNumbers.create({
-		phoneNumber: phoneNumber,
-		smsMethod: "POST",
-		smsUrl: process.env.TWILIO_SMS_URL,
-		voiceMethod: "POST",
-		voiceUrl: process.env.TWILIO_VOICE_URL,
-		statusCallbackMethod: "POST",
-		statusCallback: process.env.TWILIO_STATUS_CALLBACK,
-	});
+  // Buy phone number
+  const incomingPhoneNumber = await twilioClient.incomingPhoneNumbers.create({
+    phoneNumber: phoneNumber,
+    smsMethod: "POST",
+    smsUrl: process.env.TWILIO_SMS_URL,
+    voiceMethod: "POST",
+    voiceUrl: process.env.TWILIO_VOICE_URL,
+    statusCallbackMethod: "POST",
+    statusCallback: process.env.TWILIO_STATUS_CALLBACK,
+  });
 
-	// Transfer phone number to subaccount
-	const response = twilioClient
-		.incomingPhoneNumbers(incomingPhoneNumber.sid)
-		.update({ accountSid: twilioAccount.sid });
+  // Transfer phone number to subaccount
+  const response = twilioClient
+    .incomingPhoneNumbers(incomingPhoneNumber.sid)
+    .update({ accountSid: twilioAccount.sid });
 
-	// toll free verification
-	const tollfreeVerification =
-		await twilioClient.messaging.v1.tollfreeVerifications.create({
-			businessCity: property.city,
-			businessContactEmail: property.email,
-			businessContactFirstName: user.firstName,
-			businessContactLastName: user.lastName,
-			businessContactPhone: property.phoneNumber,
-			businessCountry: getCountryIso2(property.country),
-			businessName: property.name,
-			businessPostalCode: property.zipcode,
-			businessStateProvinceRegion: property.state,
-			businessStreetAddress: property.address,
-			businessWebsite: property.website,
-			messageVolume: "1,000",
-			notificationEmail: property.email,
-			optInType: "VERBAL",
-			optInImageUrls: ["https://onelyk-docs.s3.amazonaws.com/verbal_optin.txt"],
-			productionMessageSample:
-				"Hello, You're early checkin is approved. Use the link provided to see next steps.",
-			tollfreePhoneNumberSid: incomingPhoneNumber.sid,
-			useCaseCategories: ["CUSTOMER_CARE"],
-			useCaseSummary: "Communication with guest for hotel front desk",
-		});
-	twilioAccount.phoneNumber = incomingPhoneNumber.phoneNumber;
-	twilioAccount.phoneNumberSid = incomingPhoneNumber.sid;
-	twilioAccount.tollfreeVerificationSid = tollfreeVerification.sid;
-	await twilioAccount.save();
-	return incomingPhoneNumber;
+  // toll free verification
+  const tollfreeVerification =
+    await twilioClient.messaging.v1.tollfreeVerifications.create({
+      businessCity: property.city,
+      businessContactEmail: property.email,
+      businessContactFirstName: user.firstName,
+      businessContactLastName: user.lastName,
+      businessContactPhone: property.phoneNumber,
+      businessCountry: getCountryIso2(property.country),
+      businessName: property.name,
+      businessPostalCode: property.zipcode,
+      businessStateProvinceRegion: property.state,
+      businessStreetAddress: property.address,
+      businessWebsite: property.website,
+      messageVolume: "1,000",
+      notificationEmail: property.email,
+      optInType: "VERBAL",
+      optInImageUrls: ["https://onelyk-docs.s3.amazonaws.com/verbal_optin.txt"],
+      productionMessageSample:
+        "Hello, You're early checkin is approved. Use the link provided to see next steps.",
+      tollfreePhoneNumberSid: incomingPhoneNumber.sid,
+      useCaseCategories: ["CUSTOMER_CARE"],
+      useCaseSummary: "Communication with guest for hotel front desk",
+    });
+  twilioAccount.phoneNumber = incomingPhoneNumber.phoneNumber;
+  twilioAccount.phoneNumberSid = incomingPhoneNumber.sid;
+  twilioAccount.tollfreeVerificationSid = tollfreeVerification.sid;
+  await twilioAccount.save();
+  return incomingPhoneNumber;
 };
 
 const createSubaccount = async (propertyId) => {
-	// Check if property exists
-	const property = await Property.findById(propertyId);
-	if (!property) {
-		throw new NotFoundError("Property not found", {
-			propertyId: ["Property not found for the given id"],
-		});
-	}
-	// Check if Twilio Account already exists
-	const oldTwilioAccount = await TwilioAccount.findOne({
-		propertyId: propertyId,
-	});
-	if (oldTwilioAccount) {
-		throw new ConflictError("Twilio Account already exists", {});
-	}
-	// Create Twilio Subaccount
-	const twilioPropertySubaccount = await twilioClient.api.v2010.accounts.create(
-		{
-			friendlyName: property.name,
-		},
-	);
-	// Create Twilio Account
-	const newTwilioAccount = new TwilioAccount({
-		propertyId: property._id,
-		authToken: twilioPropertySubaccount.authToken,
-		sid: twilioPropertySubaccount.sid,
-		dateCreated: twilioPropertySubaccount.dateCreated,
-		dateUpdated: twilioPropertySubaccount.dateUpdated,
-		friendlyName: twilioPropertySubaccount.friendlyName,
-		ownerAccountSid: twilioPropertySubaccount.ownerAccountSid,
-		status: twilioPropertySubaccount.status,
-	});
-	await newTwilioAccount.save();
-	return newTwilioAccount;
+  // Check if property exists
+  const property = await Property.findById(propertyId);
+  if (!property) {
+    throw new NotFoundError("Property not found", {
+      propertyId: ["Property not found for the given id"],
+    });
+  }
+  // Check if Twilio Account already exists
+  const oldTwilioAccount = await TwilioAccount.findOne({
+    propertyId: propertyId,
+  });
+  if (oldTwilioAccount) {
+    throw new ConflictError("Twilio Account already exists", {});
+  }
+  // Create Twilio Subaccount
+  const twilioPropertySubaccount = await twilioClient.api.v2010.accounts.create(
+    {
+      friendlyName: property.name,
+    },
+  );
+  // Create Twilio Account
+  const newTwilioAccount = new TwilioAccount({
+    propertyId: property._id,
+    authToken: twilioPropertySubaccount.authToken,
+    sid: twilioPropertySubaccount.sid,
+    dateCreated: twilioPropertySubaccount.dateCreated,
+    dateUpdated: twilioPropertySubaccount.dateUpdated,
+    friendlyName: twilioPropertySubaccount.friendlyName,
+    ownerAccountSid: twilioPropertySubaccount.ownerAccountSid,
+    status: twilioPropertySubaccount.status,
+  });
+  await newTwilioAccount.save();
+  return newTwilioAccount;
 };
-
 
 /**
  * Get toll free verification status
@@ -145,49 +137,49 @@ const createSubaccount = async (propertyId) => {
  * @returns {string} status - toll free verification status
  */
 const getTollFreeVerificationStatus = async (propertyId) => {
-	// Check if property exists
-	const property = await Property.findById(propertyId);
-	if (!property) {
-		throw new NotFoundError("Property not found", {
-			propertyId: ["Property not found for the given id"],
-		});
-	}
-	// Check if twilio account exists
-	const twilioAccount = await TwilioAccount.findOne({
-		propertyId: propertyId,
-	});
-	if (!twilioAccount) {
-		throw new NotFoundError("Twilio Account not found", {
-			propertyId: ["Twilio account found for the given property id"],
-		});
-	}
-	// Get toll free verification status
-	const tollfreeVerification = await twilioClient.messaging.v1
-		.tollfreeVerifications(twilioAccount.tollfreeVerificationSid)
-		.fetch();
-	if (!tollfreeVerification.status) {
-		throw new NotFoundError("Toll Free Verification not found", {
-			propertyId: [
-				"Toll Free Verification not found for the given property id",
-			],
-		});
-	}
-	return tollfreeVerification.status;
+  // Check if property exists
+  const property = await Property.findById(propertyId);
+  if (!property) {
+    throw new NotFoundError("Property not found", {
+      propertyId: ["Property not found for the given id"],
+    });
+  }
+  // Check if twilio account exists
+  const twilioAccount = await TwilioAccount.findOne({
+    propertyId: propertyId,
+  });
+  if (!twilioAccount) {
+    throw new NotFoundError("Twilio Account not found", {
+      propertyId: ["Twilio account found for the given property id"],
+    });
+  }
+  // Get toll free verification status
+  const tollfreeVerification = await twilioClient.messaging.v1
+    .tollfreeVerifications(twilioAccount.tollfreeVerificationSid)
+    .fetch();
+  if (!tollfreeVerification.status) {
+    throw new NotFoundError("Toll Free Verification not found", {
+      propertyId: [
+        "Toll Free Verification not found for the given property id",
+      ],
+    });
+  }
+  return tollfreeVerification.status;
 };
 
 const sendAccessLink = async (propertyId, guestPhoneNumber, body) => {
-	const twilioAccount = await TwilioAccount.findOne({
-		propertyId: propertyId,
-	});
-	const twilioSubClient = twilio(accountSid, authToken, {
-		accountSid: twilioAccount.sid,
-	});
-	const message = await twilioSubClient.messages.create({
-		body: body,
-		from: `${twilioAccount.countryCode + twilioAccount.phoneNumber}`,
-		to: guestPhoneNumber,
-	});
-	return message;
+  const twilioAccount = await TwilioAccount.findOne({
+    propertyId: propertyId,
+  });
+  const twilioSubClient = twilio(accountSid, authToken, {
+    accountSid: twilioAccount.sid,
+  });
+  const message = await twilioSubClient.messages.create({
+    body: body,
+    from: `${twilioAccount.countryCode + twilioAccount.phoneNumber}`,
+    to: guestPhoneNumber,
+  });
+  return message;
 };
 
 /**
@@ -196,11 +188,11 @@ const sendAccessLink = async (propertyId, guestPhoneNumber, body) => {
  * @returns {object} messages - twilio messages object
  */
 const subaccountBilling = async (client) => {
-	const messages = await client.messages.list();
-	const bill = messages.reduce((acc, message) => {
-		return acc + Math.abs(Number(message.price));
-	}, 0);
-	return bill;
+  const messages = await client.messages.list();
+  const bill = messages.reduce((acc, message) => {
+    return acc + Math.abs(Number(message.price));
+  }, 0);
+  return bill;
 };
 
 /**
@@ -209,18 +201,18 @@ const subaccountBilling = async (client) => {
  * @returns {Promise<import('twilio').Twilio>} client - twilio client object
  */
 const getTwilioClient = async (twilioAccount) => {
-	const client = twilio(accountSid, authToken, {
-		accountSid: twilioAccount.sid,
-	});
-	return client;
+  const client = twilio(accountSid, authToken, {
+    accountSid: twilioAccount.sid,
+  });
+  return client;
 };
 
 module.exports = {
-	getPhoneNumbers,
-	buyPhoneNumber,
-	createSubaccount,
-	getTollFreeVerificationStatus,
-	sendAccessLink,
-	subaccountBilling,
-	getTwilioClient,
+  getPhoneNumbers,
+  buyPhoneNumber,
+  createSubaccount,
+  getTollFreeVerificationStatus,
+  sendAccessLink,
+  subaccountBilling,
+  getTwilioClient,
 };
