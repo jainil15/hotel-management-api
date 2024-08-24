9979e099daf444f2c83dc571f43575589de4adc1
Jainil <jainilpatel115@gmail.com>
2024-08-24 20:07:04 +0530

    Remove app contants

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-/**
-////////////////////////////////////////////////////////////////////////
-/██████╗/███╗///██╗███████╗██╗//██╗///██╗██╗//██╗/////█████╗/██████╗/██╗
-██╔═══██╗████╗//██║██╔════╝██║//╚██╗/██╔╝██║/██╔╝////██╔══██╗██╔══██╗██║
-██║///██║██╔██╗/██║█████╗//██║///╚████╔╝/█████╔╝/////███████║██████╔╝██║
-██║///██║██║╚██╗██║██╔══╝//██║////╚██╔╝//██╔═██╗/////██╔══██║██╔═══╝/██║
-╚██████╔╝██║/╚████║███████╗███████╗██║///██║//██╗////██║//██║██║/////██║
-/╚═════╝/╚═╝//╚═══╝╚══════╝╚══════╝╚═╝///╚═╝//╚═╝////╚═╝//╚═╝╚═╝/////╚═╝
- */
-
 const { Connection } = require("mongoose");
 const { createApp } = require("./src/app/app");
 const logger = require("./src/configs/winston.config");
 const { authenticateTokenSocket } = require("./src/middlewares/jwt.middleware");
 const {
-	checkPropertyAccessSocket,
+  checkPropertyAccessSocket,
 } = require("./src/middlewares/propertyaccess.middleware");
 const { Connect } = require("./src/lib/db");
-const { cmdLineLogo2 } = require("./src/constants/app.constant");
 const { Server } = require("socket.io");
 const guestSocket = require("./src/sockets/guest.socket");
 const messageSocket = require("./src/sockets/message.socket");
@@ -26,12 +15,11 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 // App
 Connect().then(() => {
-	logger.info("Database connected successfully");
+  logger.info("Database connected successfully");
 });
 try {
-	const app = createApp();
+  const app = createApp();
 } catch (error) {
-	process.exit(1);
+  process.exit(1);
 }
 // Listener
-if (process.env.NODE_ENV === "production") logger.info(cmdLineLogo2);
