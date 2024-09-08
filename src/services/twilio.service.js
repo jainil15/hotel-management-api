3bae7b7682a0eaf2f34cf5a538af16f0b985223c
jay1820 <iamjay1820@gmail.com>
2024-09-08 20:15:37 +0530

    changes in a sample message

--- src/services/twilio.service.js
+++ src/services/twilio.service.js
@@ -83,8 +83,7 @@ const buyPhoneNumber = async (propertyId, phoneNumber, user) => {
       optInType: "VERBAL",
       optInImageUrls: ["https://onelyk-docs.s3.amazonaws.com/verbal_optin.txt"],
       productionMessageSample:
-        "Hi [Guest Name], your stay at [Hotel Name] is confirmed from [Check-In Date] to [Check-Out Date]. Please reply if you have any questions.""Hi [Guest Name], your stay at [Hotel Name] is confirmed from [Check-In Date] to [Check-Out Date]. Please reply if you have any questions.",
-      tollfreePhoneNumberSid: incomingPhoneNumber.sid,
+        "Hi [Guest Name], your stay at [Hotel Name] is confirmed from [Check-In Date] to [Check-Out Date]. Please reply if you have any questions.",
       useCaseCategories: ["CUSTOMER_CARE"],
       useCaseSummary: "Communication with guest for hotel front desk",
     });
