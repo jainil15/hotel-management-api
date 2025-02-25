6b48a8fec5fdf4f1b7126119aaf5546f36fe3020
jainil15 <jainilpatel115@gmail.com>
2025-02-25 23:10:02 +0530

    Test: Send mail of failed message

--- src/utils/mail.util.js
+++ src/utils/mail.util.js
@@ -3,6 +3,57 @@ const nodemailerConfigOptions = require("../configs/nodemailer.config");
 const logger = require("../configs/winston.config");
 require("dotenv").config();
 
+const failedMessageTemplate = (propertyId, twilioAccountId, from, to) => {
+  return `
+<html>
+<body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+  <table role="presentation" style="width: 100%; height: 100%; border: 0; cellpadding: 0; cellspacing: 0; background-color: #ffffff;">
+    <tr>
+      <td align="center" style="padding: 0; margin: 0;">
+        <table role="presentation" style="width: 600px; border: 0; cellpadding: 0; cellspacing: 0; text-align: center; color: #424242;">
+          <tr>
+            <td style="font-size: xx-large; padding: 20px;">
+              Onelyk
+            </td>
+          </tr>
+          <tr>
+            <td style="font-size: x-large; padding: 10px;">
+              Failed to send message
+            </td>
+          </tr>
+          <tr>
+            <td style="font-size: large; padding: 10px;">
+              From: ${from}
+            </td>
+          </tr>
+          <tr>
+            <td style="font-size: large; padding: 10px;">
+              To: ${to}
+            </td>
+          </tr>
+          <tr>
+            <td style="font-size: large; padding: 10px;">
+              Property ID: ${propertyId}
+            </td>
+          </tr>
+          <tr>
+            <td style="font-size: large; padding: 10px;">
+              Twilio Account ID: ${twilioAccountId}
+            </td>
+          </tr>
+          <tr>
+            <td style="font-weight: lighter; color: #828282; font-size: small; padding: 5px;">
+              Onelyk, California, US
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+  `;
+};
 /**
  * Send otp to user email
  * @param {string} to - The otp
@@ -27,4 +78,5 @@ const sendMail = async (to, subject, message) => {
 
 module.exports = {
   sendMail,
+  failedMessageTemplate,
 };
