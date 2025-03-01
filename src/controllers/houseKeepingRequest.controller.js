ba6c40f664820d8373ab158799a593f3cd5799cb
jainil15 <jainilpatel115@gmail.com>
2025-03-01 17:11:47 +0530

    Fix: Open model on mail link

--- src/controllers/houseKeepingRequest.controller.js
+++ src/controllers/houseKeepingRequest.controller.js
@@ -55,8 +55,7 @@ const create = async (req, res, next) => {
     console.log(property.property.email);
     const message = houseKeepingRequestMailTemplate(guest);
     sendMail(
-      // property.property.email,
-      "jainilpatel115@gmail.com",
+      property.property.email,
       "Request for house keeping",
       message,
       "House Keeping Request",
